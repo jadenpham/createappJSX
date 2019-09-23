c7802feeda2d0a57298d0442ae88d71d310c09b7
jadenpham <tjaden.pham@gmail.com>
2019-09-23 14:41:06 -0700

    second commit, first try at using component with JSX syntax

--- src/index.js
+++ src/index.js
@@ -7,10 +7,33 @@ import React from 'react';
 //using jsx
 // const HelloReact = <h1>Hello World</h1>
 
-const HelloReact = (
-    <div className="jumbotron">
-        <h1>Hello World</h1>
-    </div>
-);
 
-ReactDOM.render(HelloReact, document.getElementById("root"));
+// const HelloReact = (
+//     <div className="jumbotron">
+//         <h1>Hello World</h1>
+//     </div>
+// );
+
+//building a function in react that is reusable
+//ES5 syntax
+// const HelloReact = function(){
+//     return(
+//         <div className="jumbotron">
+//             <h1>Hello World</h1>
+//         </div>
+//     )
+// }
+
+//building in ES6 syntax
+const HelloReact = () =>{
+    return (
+        <div className="jumbotron">
+            <h1>Hello World</h1>
+        </div>
+    )
+}
+//passing function statement in ES6
+// ReactDOM.render(HelloReact(), document.getElementById("root"));
+
+//passing function/property via JSX, same thing as above
+ReactDOM.render(<HelloReact/>, document.getElementById("root"));
