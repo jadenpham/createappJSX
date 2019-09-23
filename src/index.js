b204548f0e20275a50b1858de479548209f61413
jadenpham <tjaden.pham@gmail.com>
2019-09-23 14:26:35 -0700

    first commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react';
+//react normal call
+// const HelloReact = React.createElement("h1", null, "Hello React");
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+//using jsx
+// const HelloReact = <h1>Hello World</h1>
+
+const HelloReact = (
+    <div className="jumbotron">
+        <h1>Hello World</h1>
+    </div>
+);
+
+ReactDOM.render(HelloReact, document.getElementById("root"));
